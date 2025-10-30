134b46e722b811bf6fa8debb3f5a7f0d44edabb9
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-10-30 11:52:18 +0545

    Conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,9 @@
-import { Fragment } from "react/jsx-runtime";
+//import { Fragment } from "react/jsx-runtime";
 
 function ListGroup(){
     //const => assignable just once
     //let=> assignable again and again
-    let items = [
+    const items = [
             'New York',
             'Sans Francisco',
             'Tokyo',
@@ -11,31 +11,24 @@ function ListGroup(){
             'Paris'
     ]; 
     
-    items = [];
+    //items = [];
+    //h1 and ul cannot be returned in diff fragments so they need to be fragmented or grouped together by using div or Fragment. Frament can be implemented by empty brackets or "Fragment"
+    //Needs to be wrapped in {} because react doesnt allow normal expressions as it allows only JSX markup (for the items.wrap)
+    //the && condition is being used to make it look simpler as if ternary condition was used, it would be complex 
+    //it works in such a way that second part is always true and first part is true only when length is 0 
+    //so if length=0, no items found displayed else items    
 
-    if(items.length === 0)
         return (
         <>
             <h1>List</h1>
-            <p>No item found</p>
+            {items.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+                {items.map(item => (
+                    <li key={item}>{item}</li>
+                ))}    
+            </ul>
         </>
         )
-    return (
-        //h1 and ul cannot be returned in diff fragments so they need to be fragmented or grouped together by using div or Fragment. Frament can be implemented by empty brackets or "Fragment"
-        //Needs to be wrapped in {} because react doesnt allow normal expressions as it allows only JSX markup (for the items.wrap)
-        <>
-            <div>
-                <Fragment>
-                    <h1>List</h1>
-                    <ul className="list-group">
-                        {items.map(item => (
-                            <li key={item}>{item}</li>
-                        ))}    
-                    </ul>
-                </Fragment>
-            </div>
-        </>
-    );
 }
 
 export default ListGroup;
