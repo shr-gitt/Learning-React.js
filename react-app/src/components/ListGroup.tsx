af14906c47163046f2a55ca316aa3641bca5e93b
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-10-30 11:41:56 +0545

    Rendering Lists

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,25 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup(){
-    const items = [
+    //const => assignable just once
+    //let=> assignable again and again
+    let items = [
             'New York',
             'Sans Francisco',
             'Tokyo',
             'London',
             'Paris'
-    ];    
+    ]; 
+    
+    items = [];
 
+    if(items.length === 0)
+        return (
+        <>
+            <h1>List</h1>
+            <p>No item found</p>
+        </>
+        )
     return (
         //h1 and ul cannot be returned in diff fragments so they need to be fragmented or grouped together by using div or Fragment. Frament can be implemented by empty brackets or "Fragment"
         //Needs to be wrapped in {} because react doesnt allow normal expressions as it allows only JSX markup (for the items.wrap)
